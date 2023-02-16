8aae1e41ca909ced1022251d3986e9401e5ee598
Chaithra S <Chaithra S@DESKTOP-6EJEQN8>
2023-02-16 17:50:08 +0530

    day2 project

---
+++ Day 02/src/KIA_Motors.java
@@ -0,0 +1,20 @@
+
+public class KIA_Motors {
+
+	int noCar;
+	String Model;
+	String engine;
+	int speed;
+	
+	void display()
+	{
+		System.out.println("this is kia motors");
+	}
+	
+	public static void main(String[] args) {
+		
+		KIA_Motors k= new KIA_Motors();
+		k.display();
+	}
+
+}
